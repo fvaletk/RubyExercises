767b6f2aa3c7e338cc38a588ad2d269b12dfa8b0
Filiberto Valetk <fili.valetk@gmail.com>
2018-02-27 13:31:40 -0500

    Adding PermMissingElem exercise

---
+++ perm_missing_elem.rb
@@ -0,0 +1,42 @@
+# A zero-indexed array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+# Your goal is to find that missing element.
+
+# Write a function:
+
+#     def solution(a)
+
+# that, given a zero-indexed array A, returns the value of the missing element.
+
+# For example, given array A such that:
+#   A[0] = 2
+#   A[1] = 3
+#   A[2] = 1
+#   A[3] = 5
+
+# the function should return 4, as it is the missing element.
+
+# Assume that:
+
+#         N is an integer within the range [0..100,000];
+#         the elements of A are all distinct;
+#         each element of array A is an integer within the range [1..(N + 1)].
+
+# Complexity:
+
+#         expected worst-case time complexity is O(N);
+#         expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+
+def solution(a)
+  return 1 if (a.empty? || a.min != 1)
+  missing = a.min
+  a.sort!
+  a.each do |n|
+    missing != n ? break : missing+=1
+  end
+  missing
+end
+
+# Evaluation
+# Correctness: 100%
+# Performance: 100%
